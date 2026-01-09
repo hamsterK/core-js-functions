62f7efd77f4c3815e1860a1c90eb76f4fe5db19d
Anastasiya Krauchuk <anastasiya.krauchuk96@gmail.com>
2026-01-09 17:56:40 +0100

    feat: implement logger

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -176,8 +176,17 @@ function retry(func, attempts) {
  * cos(3.141592653589793) ends
  *
  */
-function logger(/* func, logFunc */) {
-  throw new Error('Not implemented');
+function logger(func, logFunc) {
+  return (...args) => {
+    logFunc(
+      `${func.name}(${args.map((x) => JSON.stringify(x)).join(',')}) starts`
+    );
+    const result = func(...args);
+    logFunc(
+      `${func.name}(${args.map((x) => JSON.stringify(x)).join(',')}) ends`
+    );
+    return result;
+  };
 }
 
 /**
