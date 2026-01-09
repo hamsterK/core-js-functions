c259ac222c2c878f551adb5a8972397ecf932047
Anastasiya Krauchuk <anastasiya.krauchuk96@gmail.com>
2026-01-09 18:05:35 +0100

    feat: partialUsingArguments

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -203,8 +203,10 @@ function logger(func, logFunc) {
  *   partialUsingArguments(fn, 'a','b','c')('d') => 'abcd'
  *   partialUsingArguments(fn, 'a','b','c','d')() => 'abcd'
  */
-function partialUsingArguments(/* fn, ...args */) {
-  throw new Error('Not implemented');
+function partialUsingArguments(fn, ...args) {
+  return (...args2) => {
+    return fn(...args, ...args2);
+  };
 }
 
 /**
